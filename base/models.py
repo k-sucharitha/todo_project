7b71100a7091eb45d837a38cc2abaf40b2ddc610
k-sucharitha <kakanurusucharitha@gmail.com>
2022-12-21 19:57:22 +0530

    added HTML

--- base/models.py
+++ base/models.py
@@ -7,14 +7,14 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(
-        User,on_delete=models.CASCADE,null=True,blank=True)
+        User, on_delete=models.CASCADE,null=True,blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
-    complete = models.BooleanField(default= False)
+    complete = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.title + ' ----> ' + ("Completed" if self.complete else "Pending")
 
     class Meta:
-        ordering = ['complete']     
+        ordering = ['complete']
